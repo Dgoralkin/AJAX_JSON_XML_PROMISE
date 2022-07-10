586d0cd5dbabed75e31ce6f66f7994062cd6d814
Dany Goralkin <Goralkin@Gmail.com>
2022-07-11 00:05:48 +0300

    Final json parse from txt.json

--- Script.js
+++ Script.js
@@ -1,19 +1,44 @@
 // JavaScript For index
 
-
-var div2 = document.getElementById("P2");
-const urli = 'txt.json';
+const btn = document.getElementById("B1");
 
 function myFunction() {
-  fetch(urli)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);  
-    })
-    .catch((error) => {
-        console.error(error)
-    })
-}
+    var P2 = document.getElementById("P2");
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        var txt = JSON.parse(this.responseText);
+        var data = ""
+        for (i in txt){
+            data += txt[i].name + " is a " + txt[i].species + " that likes ";
+
+            if (txt[i].foods.likes.length > 0){
+                for (j=0; j < txt[i].foods.likes.length; j++){
+                    if (j < txt[i].foods.likes.length - 1){
+                        data += txt[i].foods.likes[j] + " and ";
+                    } else {
+                        data += txt[i].foods.likes[j] + " and dislikes ";
+                    }
+                }
+            }
+            if (txt[i].foods.dislikes.length > 0){
+                for (j=0; j < txt[i].foods.dislikes.length; j++){
+                    if (j < txt[i].foods.dislikes.length - 1){
+                        data += txt[i].foods.dislikes[j] + " and ";
+                    } else {
+                        data += txt[i].foods.dislikes[j] + ".";
+                    }
+                    
+                }
+                data += "<br>";
+            }
+            P2.innerHTML = data;
+        }
+    }
+    xhttp.open("GET", "txt.json");
+    xhttp.send();
+  }
+
+
 
 
 
